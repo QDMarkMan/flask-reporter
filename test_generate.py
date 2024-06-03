ca8b1527b80ca6e0d2907b108eeef955f990dbd8
tongfu.e <tongfu.e@xtalpi.com>
2024-06-03 16:30:26 +0800

    feat: Update test_generate

--- test_generate.py
+++ test_generate.py
@@ -5,11 +5,20 @@
 *  @Description .
 **************************************************************************
 """
+import os
 from reporter import core
-
+"""
+获取报告保存位置, 属于配置文件
+"""
+def get_static_location():
+    dir_path = os.path.dirname(__file__)
+    print(dir_path)
+    return os.path.join(dir_path, "./static")
+  
 # Test
 config = core.get_config()
-core.do_generate("/static/dist/reports", core.init_data(config))
+save_path = get_static_location()
+core.do_generate(save_path, core.init_data(config))
 
 local_reports = core.get_local_reports()
 print(local_reports)
