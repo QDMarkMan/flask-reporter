81958da702e4c27d8cb4912169a504e028fe13d9
tongfu.e <tongfu.e@xtalpi.com>
2024-06-12 11:08:15 +0800

    feat: update report blocks

--- rgp/reporter/utils/report_utils.py
+++ rgp/reporter/utils/report_utils.py
@@ -11,17 +11,19 @@ import os
 
 from . import file_utils
 
-"""
-创建报告保存文件夹，这个是比较特殊的需求，属于半定制的。
-"""
-
-
 def create_report_save_folder(save_path, context):
-    # Generate report folder
+    """ Generate report folder
+    _summary_
+    Args:
+        save_path (_type_): _description_
+        context (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     now_date = dt.datetime.now().strftime("%Y-%m-%d:%H-%M")
     folder_name = context["project"]["special_name"] or context["project"]["name"]
     suffix = context["config"]["timestamp"] and now_date or ""
     report_folder = os.path.join(save_path, folder_name, suffix)
-    # Create folder
     file_utils.create_folder(report_folder)
     return report_folder
